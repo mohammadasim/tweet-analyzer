115aa17ec158df8f3b2117d61cd60e37b51631b9
Mohammad Ayub <mohammad.ayub@imagination.com>
2019-07-05 08:01:27 +0100

    added new methods

--- app.py
+++ app.py
@@ -5,11 +5,13 @@ import urllib.parse as urlparse
 import json
 
 
+consumer = oauth2.Consumer(constants.CONSUMER_KEY, constants.CONSUMER_SECRET)
+
+
 def create_authorized_user(email, first_name, last_name):
-    consumer = oauth2.Consumer(constants.CONSUMER_KEY, constants.CONSUMER_SECRET)
     client = oauth2.Client(consumer)
     response, content = client.request(constants.REQUEST_TOKEN_URL, 'POST')
-    if response != 200:
+    if response.status != 200:
         print('An error has occured')
     request_token = dict(urlparse.parse_qsl(content.decode('utf-8')))
     print('Please click the following link to get a pin')
@@ -25,6 +27,17 @@ def create_authorized_user(email, first_name, last_name):
     return user
 
 
+def create_twitter_query(email, twitter_url, request_verb):
+    user = User.find_by_email(email)
+    authorized_token = oauth2.Token(user.oauth_token, user.oauth_token_secret)
+    authorized_client = oauth2.Client(consumer, authorized_token)
+    response, content = authorized_client.request(twitter_url, request_verb)
+    if response.status != 200:
+        print('An error has happened. Received this status code {}'.format(response.status))
+    result = json.loads(content.decode('utf-8'))
+    return result
 
-def create_twitter_query(email):
-    pass
+
+result = create_twitter_query('asim.kdr@gmail.com', 'https://api.twitter.com/1.1/search/tweets.json?q=brexit', 'GET')
+for element in result['statuses']:
+    print(element['text'])
